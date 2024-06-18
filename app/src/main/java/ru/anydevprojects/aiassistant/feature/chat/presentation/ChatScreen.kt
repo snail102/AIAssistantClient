93875f004d2c0ec7904f10ce7389ac79e3a741dd
Nikolay Kozhevnikov <kozhevnikov_n0704@core.ufanet.ru>
2024-06-18 09:36:24 +0500

    исправлен ui для ввода теста запроса

--- app/src/main/java/ru/anydevprojects/aiassistant/feature/chat/presentation/ChatScreen.kt
+++ app/src/main/java/ru/anydevprojects/aiassistant/feature/chat/presentation/ChatScreen.kt
@@ -1,13 +1,17 @@
 package ru.anydevprojects.aiassistant.feature.chat.presentation
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -25,6 +29,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -34,9 +39,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
@@ -194,9 +201,10 @@ private fun BottomInputControl(
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .padding(16.dp)
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        TextField(
+        OutlinedTextField(
             modifier = Modifier
                 .fillMaxWidth()
                 .weight(1f),
@@ -204,8 +212,13 @@ private fun BottomInputControl(
             onValueChange = onMessageChange
         )
 
+        Spacer(
+            modifier = Modifier.width(16.dp)
+        )
+
         IconButton(
-            modifier = Modifier.size(24.dp),
+            modifier = Modifier
+                .size(24.dp),
             enabled = isAvailableSendBtn,
             onClick = onClickSendBtn
         ) {
@@ -214,5 +227,22 @@ private fun BottomInputControl(
                 contentDescription = "send"
             )
         }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewBottomInputControl() {
+    Box(
+        modifier = Modifier.background(color = Color.White)
+    ) {
+        BottomInputControl(
+            isAvailableSendBtn = true,
+            message = "133333",
+            onMessageChange = {
+            },
+            onClickSendBtn = {
+            }
+        )
     }
 }
